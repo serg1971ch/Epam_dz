718cc40286c16ac9aa5d1c697b33970bd0d197eb
serg1971ch <serg1971ch@yandex.ru>
2022-02-06 03:12:38 +0500

    it's my first app

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,7 @@
 public class HelloWorld {
-
-
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + ", " + world + " !");
+    }
 }
