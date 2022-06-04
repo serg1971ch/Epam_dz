ecd8768feadfb99d5d5ceed96039e60229f398ea
serg1971ch12@gmail.com <https://gitlab.skillbox.ru>
2022-06-04 19:04:44 +0500

    robot_market_html

--- robot_market_html/src/js/auth.js
+++ robot_market_html/src/js/auth.js
@@ -1,22 +1,30 @@
-const btn = document.getElementById('2');
-const enter = document.getElementById("1");
-const out = document.getElementById('3');
-const menu = document.querySelector('.menu');
-const item = document.getElementsByTagName('ul.li');
 
-
-btn.onclick = function () {
-    // menu.insertAdjacentHTML('afterend' , '<li id=3 ><a class="out" href="#">Выйти</a></li>');
-    // out.style.background = 'blue';
-    enter.remove();
-    window.location.href = 'index.html';
+// const enter = document.getElementById("1");
+// const out = document.getElementById('3');
+// const menu = document.querySelector('.menu');
+const item = document.getElementsByClassName('list');
+const btn = document.getElementsByClassName('button');
+const delItem = function () {
+    for (let i = 0; i < item.length; i++) {
+        console.log(item[i]);
+        if (i === 4) {
+            item[i].remove();
+        }
+    }
 }
+delItem();
+// btn.onclick = function () {
+//     // menu.insertAdjacentHTML('afterend' , '<li id=3 ><a class="out" href="#">Выйти</a></li>');
+//     // out.style.background = 'blue';
+//     enter.remove();
+//     window.location.href = 'index.html';
+// }
 
-// btn.addEventListener('click', logIn);
+// btn.addEventListener('click', delItem);
 
 // btn.addEventListener('onclick', () => {
 //       menu.insertAdjacentText('afterend','<li id=3 ><a class="out" href="auth.html">Выйти</a></li>');
 //       enter.remove();
-    // list.replaceWith(h1);
-    // h1.classList.add("_name");
+// list.replaceWith(h1);
+// h1.classList.add("_name");
 // })
