7b9c116ae6f723a6ea8dc4e25c1249a7a68414e3
serg1971ch <serg1971ch@yandex.ru>
2022-06-05 06:21:11 +0500

    robot_market_html

--- robot_market_html/src/js/auth.js
+++ robot_market_html/src/js/auth.js
@@ -1,18 +1,29 @@
-
-// const enter = document.getElementById("1");
-// const out = document.getElementById('3');
-// const menu = document.querySelector('.menu');
-const item = document.getElementsByClassName('list');
-const btn = document.getElementsByClassName('button');
-const delItem = function () {
-    for (let i = 0; i < item.length; i++) {
-        console.log(item[i]);
-        if (i === 4) {
-            item[i].remove();
-        }
-    }
+if (typeof window !== 'undefined') {
+    console.log('You are on the browser')
+} else {
+    console.log('You are on the server')
 }
-delItem();
+const list = $(".menu");
+const leaf = $('li#3.list');
+const menu = document.querySelector('.menu');
+// const item = document.getElementsByClassName('list');
+const btn = $('input.button');
+
+btn.onclick = function () {
+    alert('Спасибо');
+    console.log("просто офигеть!")
+}
+// function ex() {
+//     for (let i = 0; i < leaf.length; i++) {
+//         console.log(leaf[i]);
+//         if (i === 2) {
+//             console.log(leaf);
+//             return list.innerHTML ='<li id=3 class="list"><a class="out" href="#">Выйти</a></li>';
+//         }
+//     }
+// }
+//
+// ex()
 // btn.onclick = function () {
 //     // menu.insertAdjacentHTML('afterend' , '<li id=3 ><a class="out" href="#">Выйти</a></li>');
 //     // out.style.background = 'blue';
@@ -20,7 +31,9 @@ delItem();
 //     window.location.href = 'index.html';
 // }
 
-// btn.addEventListener('click', delItem);
+// btn.addEventListener('click', ex());
+btn.addEventListener('click', () =>
+    alert('Спасибо'));
 
 // btn.addEventListener('onclick', () => {
 //       menu.insertAdjacentText('afterend','<li id=3 ><a class="out" href="auth.html">Выйти</a></li>');
